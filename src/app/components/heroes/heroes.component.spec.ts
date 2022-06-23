9d831f3a2a8c14002423d006552c42ff5d67c4c1
Lukas Gorniak <lukas.gorniak@generic.de>
2022-06-23 12:02:59 +0000

    Merged PR 12: Add Hero Editor
    You used the CLI to create a **second HeroesComponent**
    You displayed the HeroesComponent by **adding it to the** AppComponent **shell**
    You applied the **UppercasePipe** to format the name
    You used **two-way data binding** with the _ngModel_ directive
    You learned about the **AppModule**
    You imported the **FormsModule** in the AppModule so that Angular would recognize and apply the _ngModel_ directive
    You learned the **importance of declaring components** in the AppModule and appreciated that the CLI declared it for you
    You learned how to **test your code** before creating a pull-request with _ng test_
    
    Related work items: #55

---
+++ src/app/components/heroes/heroes.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroesComponent } from './heroes.component';
+import { FormsModule } from '@angular/forms';
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let fixture: ComponentFixture<HeroesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [HeroesComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HeroesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
