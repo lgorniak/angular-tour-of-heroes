f875e2c1be97e66c8dc094b6199b16bf4fd9d4ca
Lukas Gorniak <lukas.gorniak@generic.de>
2022-06-24 08:56:59 +0200

    fix tests

--- src/app/components/hero-detail/hero-detail.component.spec.ts
+++ src/app/components/hero-detail/hero-detail.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroDetailComponent } from './hero-detail.component';
+import { FormsModule } from '@angular/forms';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
@@ -7,6 +8,7 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [FormsModule],
       declarations: [HeroDetailComponent],
     }).compileComponents();
 
