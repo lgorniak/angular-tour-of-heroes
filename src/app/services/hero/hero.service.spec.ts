4896c5e3d92768d845f2707cbc2eec1bf1494a37
Lukas Gorniak <lukas.gorniak@generic.de>
2022-06-30 10:45:24 +0200

    remove HttpClient from test

--- src/app/services/hero/hero.service.spec.ts
+++ src/app/services/hero/hero.service.spec.ts
@@ -1,18 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { HeroService } from './hero.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { MessageService } from '../messages/message.service';
 
 describe('HeroService', () => {
   let heroService: HeroService;
-  let httpClient: HttpClient;
-  let messageService: MessageService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [HeroService],
+      providers: [HeroService, MessageService],
     });
 
     heroService = TestBed.inject(HeroService);
