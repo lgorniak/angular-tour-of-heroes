480cade04ce76e6870c52d2bcb7c958e7b0f0bf8
Lukas Gorniak <lukas.gorniak@generic.de>
2022-07-01 09:34:04 +0200

    remove component test dependency on communication with http protocol

--- src/app/components/hero-detail/hero-detail.component.spec.ts
+++ src/app/components/hero-detail/hero-detail.component.spec.ts
@@ -1,16 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroDetailComponent } from './hero-detail.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HeroService } from '../../services/hero/hero.service';
+import { of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
 
   beforeEach(async () => {
+    const heroServiceSpy = jasmine.createSpyObj<HeroService>([
+      'getHero',
+      'updateHero',
+    ]);
+    heroServiceSpy.getHero.and.returnValue(of({ id: 1, name: 'test' }));
+    heroServiceSpy.updateHero.and.returnValue(of({}));
+
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
+      imports: [RouterTestingModule, FormsModule],
       declarations: [HeroDetailComponent],
+      providers: [{ provide: HeroService, useValue: heroServiceSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeroDetailComponent);
--- src/app/components/heroes/heroes.component.spec.ts
+++ src/app/components/heroes/heroes.component.spec.ts
@@ -1,16 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroesComponent } from './heroes.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HeroService } from '../../services/hero/hero.service';
+import { of } from 'rxjs';
 
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
 
   beforeEach(async () => {
+    const heroServiceSpy = jasmine.createSpyObj<HeroService>(['getHeroes']);
+    heroServiceSpy.getHeroes.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
-      imports: [AppRoutingModule, HttpClientTestingModule],
+      imports: [AppRoutingModule],
       declarations: [HeroesComponent],
+      providers: [{ provide: HeroService, useValue: heroServiceSpy }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeroesComponent);
